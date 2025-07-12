22e473c5a036844ab325ffd6163c934e5cad6ea5
Zeeshan Khan <zeeshan.saeed8323@gmail.com>
2025-07-12 23:16:23 +0500

    Create 04_switch_case.php

---
+++ 04_switch_case.php
@@ -0,0 +1,13 @@
+<?php
+$day = "Monday";
+switch ($day) {
+    case "Monday":
+        echo "Start of the week!";
+        break;
+    case "Friday":
+        echo "Weekend is near.";
+        break;
+    default:
+        echo "Just another day.";
+}
+?>
