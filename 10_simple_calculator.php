443897d261f8cb9d5a083617895ee72a40113ff1
Zeeshan Khan <zeeshan.saeed8323@gmail.com>
2025-07-12 23:20:50 +0500

    Create 10_simple_calculator.php

---
+++ 10_simple_calculator.php
@@ -0,0 +1,8 @@
+<?php
+$a = 10;
+$b = 5;
+echo "Sum: " . ($a + $b) . "<br>";
+echo "Difference: " . ($a - $b) . "<br>";
+echo "Product: " . ($a * $b) . "<br>";
+echo "Quotient: " . ($a / $b) . "<br>";
+?>
